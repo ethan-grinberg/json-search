c3a5bb08eb19e7ceae67034e3cf4b2957df3bb57
ethanbg2 <ethanbg2@illinois.edu>
2021-02-05 21:39:04 -0600

    Added loadJson from file method.

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,31 +2,18 @@ package com.example;
 
 import com.example.models.Book;
 import com.example.models.Books;
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-import com.google.gson.stream.JsonReader;
-
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
-import java.lang.reflect.Type;
-import java.util.ArrayList;
+import java.util.List;
 
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Gson gson = new Gson();
-        File file = new File("C:\\Users\\ethan\\Documents\\School\\CS126\\Json\\src\\main\\resources\\classics.json");
-        JsonReader reader = new JsonReader(new FileReader(file));
-        Type booksType = new TypeToken<ArrayList<Book>>(){}.getType();
-        ArrayList<Book> bookArrayList = gson.fromJson(reader, booksType);
-        Books books = new Books(bookArrayList);
+        List<Book> books = Books.loadBooksFromJsonFile(
+                "C:\\Users\\ethan\\Documents\\School\\CS126\\Json\\src\\main\\resources\\classics.json");
 
-        System.out.println(books.getBooks().size());
-        for (Book book : books.getBooks()) {
+        System.out.println(books.size());
+        for (Book book : books) {
             System.out.println(book);
         }
-
-
     }
 }
--- src/main/java/com/example/models/Books.java
+++ src/main/java/com/example/models/Books.java
@@ -1,19 +1,30 @@
 package com.example.models;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.lang.reflect.Type;
 import java.util.List;
 
 public class Books {
-    private List<Book> books;
     //TODO javadoc comment
-    public Books(List<Book> setBooks) {
-        if (setBooks == null) {
+
+    public static List<Book> loadBooksFromJsonFile(String filePath) throws FileNotFoundException {
+        if (filePath == null) {
             throw new IllegalArgumentException();
         }
-        books = setBooks;
+        Gson gson = new Gson();
+        File file = new File(filePath);
+        JsonReader reader = new JsonReader(new FileReader(file));
+        Type booksType = new TypeToken<List<Book>>(){}.getType();
+        List<Book> bookList = gson.fromJson(reader, booksType);
+
+        return bookList;
     }
-    //TODO javadoc comment
-    public List<Book> getBooks() {
-        return books;
-    }
+
 
 }
